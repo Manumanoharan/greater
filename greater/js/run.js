76d14501c8f1c5fc99c870b39160e81ee5d1e71a
Manu <90511900+Manumanoharan@users.noreply.github.com>
2022-07-16 19:31:27 +0530

    Add files via upload

---
+++ greater/js/run.js
@@ -0,0 +1,18 @@
+function numbers(a)
+{
+   if(a>10)
+   {
+      var numbers='a is greaterthan 10';
+   }
+   else 
+   {
+      var numbers='a is not a greaterthan 10';
+   }
+   return numbers;
+
+}
+var a=6;
+
+
+var number=numbers(a);
+console.log (number);
